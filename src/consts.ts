c69de1843894e306508f298a5a476c4759ca310f
Vidur Butalia <vidurbutalia@gmail.com>
2025-12-27 18:38:16 +0530

    feat: re-init

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "vidur.dev";
+export const SITE_DESCRIPTION = "go away, stop reading this";
